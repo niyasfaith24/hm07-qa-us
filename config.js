c5d80e83ffd00e2d4fc1da366f41fe4b98a9098a
naya <practicum_student@gmail.com>
2024-09-02 08:04:01 -0700

    my second commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-e203fbd9-b913-4b87-a046-4a4fb0e2e745.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-08f95d00-a943-4690-93d0-eb23a0e9d16c.containerhub.tripleten-services.com"
 }
--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,12 +1,34 @@
-// eslint-disable-next-line no-undef
+
+
 const config = require('../config');
 
-test('', async () => {
+test('status should be 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
-			method: 'DELETE',
-		});
-	} catch (error) {
-		console.error(error);
-	}
+        // Make request
+        const response = await fetch(`${config.API_URL}/api/v1/kits/3`, {
+            method: 'DELETE',
+        });
+
+        // Check status code
+        expect(response.status).toBe(200);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+test('response body should contain expected data', async () => {
+    try {
+        // Make request
+        const response = await fetch(`${config.API_URL}/api/v1/kits/3`, {
+            method: 'DELETE',
+        });
+
+        // Extract response body
+        const responseBody = await response.json();
+        
+        // Check response body
+        expect(responseBody).toEqual({ ok: true });
+    } catch (error) {
+        console.error(error);
+    }
 });
