146eddca87032951adbead0759b2e36f72a1d045
naya <practicum_student@gmail.com>
2024-09-03 11:15:39 -0700

    my second commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-e5cb1d44-e2c3-4d0c-b64e-20f8e9b47d80.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-3dd833ec-06b4-419a-9bfc-8f38102a62f4.containerhub.tripleten-services.com"
 }
